9f3f7ae67d8b818698c407779ad6c7a1938d84c7
newnetmp3 <pcservices.ccity@gmail.com>
2026-09-17 21:43:48 -0400

    Synchronize Wayland wheel zoom redraws

--- src/carvefoundry/ui/viewport.py
+++ src/carvefoundry/ui/viewport.py
@@ -353,7 +353,7 @@ class MeshViewport(_GpuMeshViewport):
         if steps:
             self.zoom *= 1.20**steps
             self.zoom = max(self.MIN_ZOOM, min(self.MAX_ZOOM, self.zoom))
-            self.update(self.rect())
+            self._interactive_redraw()
         event.accept()
 
 
