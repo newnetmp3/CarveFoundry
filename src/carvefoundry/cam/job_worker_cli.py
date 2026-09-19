78711d4f4c51d339767217376635aea86660cdc9
newnetmp3 <pcservices.ccity@gmail.com>
2026-09-19 00:19:40 -0400

    Separate process entry point from pickled job classes

---
+++ src/carvefoundry/cam/job_worker_cli.py
@@ -0,0 +1,7 @@
+"""Execute one isolated job without redefining pickled request classes."""
+
+from carvefoundry.cam.job_process import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
