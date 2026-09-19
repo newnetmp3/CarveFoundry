ab7d1103f3d3665f9b99c3b42ac89c74241a8dbd
newnetmp3 <pcservices.ccity@gmail.com>
2026-09-18 23:35:43 -0400

    Apply rustfmt formatting

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,7 +6,10 @@ use pyo3::types::PyModule;
 
 #[pymodule]
 fn _native(module: &Bound<'_, PyModule>) -> PyResult<()> {
-    module.add_function(wrap_pyfunction!(heightfield::rasterize_top_surface, module)?)?;
+    module.add_function(wrap_pyfunction!(
+        heightfield::rasterize_top_surface,
+        module
+    )?)?;
     module.add_function(wrap_pyfunction!(contact::compensate_height_field, module)?)?;
     Ok(())
 }
