f1061225ad1bf046e73f42a77b66524b038d7509
newnetmp3 <pcservices.ccity@gmail.com>
2026-09-18 22:49:27 -0400

    Use collections.abc Callable for CAM progress

--- src/carvefoundry/cam/heightfield.py
+++ src/carvefoundry/cam/heightfield.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from math import ceil
-from typing import Callable
+from collections.abc import Callable
 
 import numpy as np
 import trimesh
