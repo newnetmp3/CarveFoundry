71becbbb8b135b33cb304325ad57d28b0245081d
newnetmp3 <pcservices.ccity@gmail.com>
2026-09-21 05:13:21 -0400

    Sort viewport refactor test imports

--- tests/test_ui_refactor.py
+++ tests/test_ui_refactor.py
@@ -10,7 +10,6 @@ from carvefoundry.ui.contextual_tool_state import ToolOptionsState
 from carvefoundry.ui.job_utility_actions import JobUtilityActionsMixin
 from carvefoundry.ui.main_window import MainWindow
 from carvefoundry.ui.native_viewport import _NativeOpenGLViewport
-from carvefoundry.ui.viewport import MeshViewport as PublicMeshViewport
 from carvefoundry.ui.project_edit_actions import ProjectEditActionsMixin
 from carvefoundry.ui.ribbon_action_state import RibbonActionStateMixin
 from carvefoundry.ui.ribbon_actions import RibbonActionsMixin
@@ -19,6 +18,7 @@ from carvefoundry.ui.ribbon_design_tools import RibbonDesignToolsMixin
 from carvefoundry.ui.ribbon_machine_actions import RibbonMachineActionsMixin
 from carvefoundry.ui.smart_value_actions import SmartValueActionsMixin
 from carvefoundry.ui.view_simulation_actions import ViewSimulationActionsMixin
+from carvefoundry.ui.viewport import MeshViewport as PublicMeshViewport
 from carvefoundry.ui.viewport_geometry import ViewportGeometryMixin
 from carvefoundry.ui.viewport_interactions import ViewportInteractionMixin
 from carvefoundry.ui.viewport_widget import MeshViewport
